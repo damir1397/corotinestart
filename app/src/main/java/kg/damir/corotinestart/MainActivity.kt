a72e9baab0a69692712a26f8e62c1a3d0d75045d
Damir <damir.danzo@gmail.com>
2023-02-24 18:52:27 +0600

    thread runOnUiThread

--- app/src/main/java/kg/damir/corotinestart/MainActivity.kt
+++ app/src/main/java/kg/damir/corotinestart/MainActivity.kt
@@ -3,6 +3,8 @@ package kg.damir.corotinestart
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.widget.Toast
 import androidx.core.view.isVisible
 import kg.damir.corotinestart.databinding.ActivityMainBinding
@@ -13,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private val handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +26,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadData() {
+        Log.d("MainActivity","Load start $this")
         with(binding) {
             progress.isVisible = true
             buttonLoad.isEnabled = false
@@ -32,6 +36,7 @@ class MainActivity : AppCompatActivity() {
                     tvTemperature.text = it.toString()
                     progress.isVisible = false
                     buttonLoad.isEnabled = true
+                    Log.d("MainActivity","Load finish $this")
                 }
 
             }
@@ -43,18 +48,26 @@ class MainActivity : AppCompatActivity() {
     private fun loadCity(callBack: (String) -> Unit) {
         thread {
             Thread.sleep(5000)
-            callBack.invoke("Moscow")
+            runOnUiThread {
+                callBack.invoke("Moscow")
+            }
+
         }
     }
 
     @SuppressLint("StringFormatInvalid")
     private fun loadTemperature(city: String, callBack: (Int) -> Unit) {
         thread {
-            Toast.makeText(this, getString(R.string.loading_temperature_toast, city), Toast.LENGTH_SHORT)
-                .show()
-            Thread.sleep(5000)
-            callBack.invoke(17)
+            runOnUiThread {
+                Toast.makeText(
+                    this,
+                    getString(R.string.loading_temperature_toast, city),
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+                Thread.sleep(5000)
+                callBack.invoke(17)
+            }
         }
-
     }
 }
